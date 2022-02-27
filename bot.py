cdf9db7bfc36c632e6e8fe865e1406a7217daf3f
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-02-27 21:16:06 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,11 @@
-# (c) HeimanPictures
-
+import os
 import logging
+from config import Config
+import pyrogram
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-import os
-from config import Config
-
-import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if __name__ == "__main__" :
@@ -16,7 +13,7 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        "Mdisk-Bot",
+        "Multi-ClientBot",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
