3b500f53a57fefc304ef9d5e78c50a052774c89a
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-02-27 21:35:27 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-    API_ID = int(os.environ.get("API_ID", 123456))
-
-    API_HASH = os.environ.get("API_HASH", "")
-    
-    API_KEY = os.environ.get("API_KEY", "")
-
+    BOT_TOKEN = "5075101927:AAFx07Vo2mGnnG6KGwrhFikFYQJAxiXx9O4"
+    BOT_TOKEN2 = "5061228375:AAGLT2aCPAYF2nLr3Z5WB6GnunHi_o-FLI8"
+    API_ID = int(18282440)
+    API_HASH = "d26ba8adb2a566618bddc31435d4f67e"
+    #API_KEY = 
     # AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
-
     # PRIVATE = bool(os.environ.get("PRIVATE", ""))
